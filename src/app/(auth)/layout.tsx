e7b5ec98a3d587341d68a980559a1bc46ce7372b
PHANI KUMAR REDDY <phaniksrm@gmail.com>
2025-10-08 09:29:20 +0000

    am not able to login now

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,11 +1,29 @@
+
+'use client';
+
 import Link from "next/link";
 import { Logo } from "@/components/logo";
+import { useAuth, initiateAnonymousSignIn, useUser } from "@/firebase";
+import React from "react";
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const auth = useAuth();
+  const { user, isUserLoading } = useUser();
+
+  React.useEffect(() => {
+    // If user is not logged in and we are not in the process of loading the user
+    // sign them in anonymously. This is required for the user to be able to
+    // have the permissions to even sign up or log in.
+    if (!isUserLoading && !user) {
+      initiateAnonymousSignIn(auth);
+    }
+  }, [isUserLoading, user, auth]);
+
+
   return (
     <div className="flex min-h-screen flex-col">
        <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
