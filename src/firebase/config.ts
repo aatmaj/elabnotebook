313b815d8ca1534f5153c0f6db4d294fbbc2c8e9
PHANI KUMAR REDDY <phaniksrm@gmail.com>
2025-10-08 09:16:05 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4113947328-fd5fb",
+  "appId": "1:518194464703:web:7f4853f391e2d2e059c30a",
+  "apiKey": "AIzaSyBGazdR_i04DVR3gurYovP7MnLIEsNN3qk",
+  "authDomain": "studio-4113947328-fd5fb.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "518194464703"
+};
